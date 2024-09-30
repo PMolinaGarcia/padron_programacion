94fc82dbbd327b6bfaf8e51a939de3842dd902ae
PABLO MOLINA GARCÍA <pablo.molinagarcia1@usp.ceu.es>
2024-09-30 12:13:49 +0200

    Clases de dominio v1

---
+++ src/dominio/Habitante.java
@@ -0,0 +1,25 @@
+package dominio;
+public class Habitante{
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	public Habitante(){}
+	public Habitante(String nombre_,String apellido1_,String apellido2_){
+		nombre=nombre_;
+		apellido1=apellido1_;
+		apellido2=apellido2_;
+		}
+
+	public String getNombre(){
+		return nombre;
+		}
+
+	public void setNombre(String nombre_){
+		nombre=nombre_;
+		}
+
+	public String toString(){
+		return "Nombre: " + nombre + " " + apellido1 + " " + apellido 2;
+		}
+
+}
