a7d1d8efc3814ff0211ccc947318e963cdf5d7f2
Pablo Molina García <pablo.molinagarcia1@usp.ceu.es>
2024-10-03 09:14:10 +0200

    Add files via upload

---
+++ src/dominio/OficinaPadron.java
@@ -0,0 +1,64 @@
+public static class OficinaPadron{//Hemos modificado la clase a "static" para que el método estático pueda acceder a ella
+            private ArrayList<Habitante> habitantesPadron = new
+            ArrayList<>();
+                    
+              public OficinaPadron(){
+                    cargarHabitantes();
+                }
+              public void annadir(Habitante habitante){
+                habitantesPadron.add(habitante);
+                volcarContactos();
+                }
+              public ArrayList getHabitantesPadron(){
+                return habitantesPadron;
+                }
+              public int calcularNumeroHabitantes(){
+                int suma = 0;
+                for (Habitante habitante : habitantesPadron){
+                    suma++;
+                }
+                    return suma;
+                }
+              private void cargarHabitantes(){
+                try{
+                    File fichero = new File ("padron.csv");
+                    fichero.createNewFile();
+                    Scanner sc = new Scanner(fichero);
+                    sc.useDelimiter(",|\n");
+                    while(sc.hasNext()){
+                        Habitante habitante = new Habitante (sc.next(),sc.next(),sc.next());
+                        habitantesPadron.add(habitante);
+                    }
+                    sc.close();
+                }catch (IOException ex){
+                    System.out.println("No hay habitantes inscritos");
+                }
+                }
+               public void volcarContactos(){
+                   try{
+                       FileWriter fw = new FileWriter("padron.csv");
+                       for (Habitante habitante : habitantesPadron){
+                           fw.write(habitante.getNombre() + "," + habitante.getApellido1() + "," + habitante.getApellido2() + "n");
+                       }
+                       fw.close();
+                    }catch (IOException ex){
+                        System.err.println(ex);
+                    }
+                   }
+                }
+    
+public class Comprobacion{
+              public static void realizarComprobacion(){
+                OficinaPadron padron = new OficinaPadron();
+                ArrayList <Habitante> habitantes = padron.getHabitantesPadron();
+                for (Habitante habitante : habitantes){
+                    
+                        System.out.println( 
+                            habitante.getNombre() + " " +
+                            habitante.getApellido1() + " " +
+                            habitante.getApellido2());
+                    
+                }
+                System.out.println("El número total de habitantes es "+ padron.calcularNumeroHabitantes());
+                }
+            } 
